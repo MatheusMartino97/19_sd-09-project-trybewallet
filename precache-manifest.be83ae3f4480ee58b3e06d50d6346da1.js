029f4428e294fd91e93e8e0ca6ec278a3fa92584
Matheus Martino <matheus.martino97@gmail.com>
2021-04-05 20:04:49 -0300

    Updates

---
+++ precache-manifest.be83ae3f4480ee58b3e06d50d6346da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74a0daf153717f56cd7091b56611103",
+    "url": "/19_sd-09-project-trybewallet/index.html"
+  },
+  {
+    "revision": "6815fda2819fa32d1959",
+    "url": "/19_sd-09-project-trybewallet/static/css/main.3cd14b69.chunk.css"
+  },
+  {
+    "revision": "8f80ea8db5a2ab22c23a",
+    "url": "/19_sd-09-project-trybewallet/static/js/2.361d7263.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/19_sd-09-project-trybewallet/static/js/2.361d7263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6815fda2819fa32d1959",
+    "url": "/19_sd-09-project-trybewallet/static/js/main.9e188d69.chunk.js"
+  },
+  {
+    "revision": "3d0295fb95275dda4886",
+    "url": "/19_sd-09-project-trybewallet/static/js/runtime-main.e9afa88c.js"
+  }
+]);
